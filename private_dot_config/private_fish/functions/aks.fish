ca5c7534cb5a4455ba330a1bc015b4e01ba5e78b
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2023-10-12 18:30:13 +0530

    fish: adds aks function

---
+++ private_dot_config/private_fish/functions/aks.fish
@@ -0,0 +1,10 @@
+function aks
+	switch $argv[1]
+	case "stern"
+		KUBECONFIG=$HOME/.kube/aks-config stern $argv[2..]
+	case "kubens"
+		KUBECONFIG=$HOME/.kube/aks-config kubens $argv[2..]
+	case "*"
+		KUBECONFIG=$HOME/.kube/aks-config kubectl $argv[1..]
+	end
+end
