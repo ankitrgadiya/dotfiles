8c25e91ecb8134198583938d073d14d9db8d8bd2
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2023-11-15 10:21:37 +0530

    fish: add aks k9s handling

--- private_dot_config/private_fish/functions/aks.fish
+++ private_dot_config/private_fish/functions/aks.fish
@@ -4,6 +4,8 @@ function aks
 		KUBECONFIG=$HOME/.kube/aks-config stern $argv[2..]
 	case "kubens"
 		KUBECONFIG=$HOME/.kube/aks-config kubens $argv[2..]
+	case "k9s"
+		KUBECONFIG=$HOME/.kube/aks-config k9s $argv[2..]
 	case "*"
 		KUBECONFIG=$HOME/.kube/aks-config kubectl $argv[1..]
 	end
