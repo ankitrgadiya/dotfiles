9cb67ca04db25de0344714275224bdb26956bb05
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2024-02-21 19:47:22 +0530

    emacs: remove personal and work configuration

--- private_dot_config/emacs/modules/arg-pers.el
+++ private_dot_config/emacs/modules/arg-pers.el
@@ -1,8 +1,1 @@
 ;;; arg-pers.el -- Personal Configurations
-
-(use-package arg-status
-  :config
-  (require 'exec-path-from-shell)
-  (setq arg-status-path (exec-path-from-shell-getenv "ARG_STATUS_PATH"))
-  (evil-leader/set-key
-	"ws" 'arg-status))
--- private_dot_config/emacs/modules/arg-work.el
+++ private_dot_config/emacs/modules/arg-work.el
@@ -1,10 +1,1 @@
 ;;; arg-work.el -- Work Configurations
-
-(use-package arg-hwil
-  :config
-  (arg-hwil-setup)
-  (evil-leader/set-key
-	"he" 'arg-hwil-eshell-device))
-
-(use-package arg-stern
-  :commands arg-stern-follow)
