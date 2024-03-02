0b557a5e816fee9a96f73f74a277ba4d644f13e3
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2024-03-02 17:36:31 +0530

    emacs: adds org configuration

--- private_dot_config/emacs/modules/arg-org.el
+++ private_dot_config/emacs/modules/arg-org.el
@@ -4,25 +4,54 @@
   :ensure t
   :config
   ;; Global Configuration
-  (setq org-directory "~/.local/share/org")
+  (setq org-directory "~/.local/share/org"
+		org-tags-column 0
+		org-fold-catch-invisible-edits 'show-and-error
+		org-insert-heading-respect-content t)
 
   ;; Babel Configuration
   (setq org-src-fontify-natively t
 		org-src-tab-acts-natively t)
 
+  ;; Capture Configuration
+  (setq org-default-notes-file (concat org-directory "/inbox.org")
+		org-capture-templates
+		'(("w" "Work Task" entry (file+headline "~/.local/share/org/inbox.org" "Work")
+		   "* TODO %?\n  %i\n  %a")
+		  ("h" "Home Task" entry (file+headline "~/.local/share/org/inbox.org" "Home")
+		   "* TODO %?\n  %i\n  %a")))
+
   ;; Agenda Configuration
   (setq org-agenda-files '("inbox.org")
 		org-todo-keywords '((sequence "BACKLOG" "TODO" "IN PROGRESS" "BLOCKED"
-									  "|" "DONE")))
+									  "|" "DONE"))
+		org-log-done 'time)
   (require 'evil)
-  (evil-define-key 'normal 'global (kbd "<leader>aa") 'org-agenda))
+  (evil-define-key 'normal 'global
+	(kbd "<leader>aa") 'org-agenda
+	(kbd "<leader>ao") 'org-cycle-agenda-files
+	(kbd "<leader>ac") 'org-capture))
 
 ;; `org-present' implements Presentations for org-mode documents.
 (use-package org-present
   :ensure t
   :config
-  (add-hook 'org-present-mode-hook 'org-present-big)
-  (add-hook 'org-present-mode-quit-hook 'org-present-small)
+  (setq org-present-text-scale 3)
 
   (require 'evil)
-  (evil-define-key 'normal 'global (kbd "<leader>op") 'org-present))
+  (evil-define-key 'normal 'global (kbd "<leader>op") 'org-present)
+
+  (add-hook 'org-present-mode-hook 'org-present-big)
+  (add-hook 'org-present-mode-quit-hook 'org-present-small))
+
+;; `org-modern' is the eye-candy package for Org-mode.
+(use-package org-modern
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook #'org-modern-mode)
+  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  (set-face-attribute 'org-modern-symbol nil :font "Iosevka" :weight 'bold :height 140)
+  (set-face-attribute 'org-document-title nil :font "Iosevka" :weight 'bold :height 1.3)
+  (setq org-hide-emphasis-markers t
+		org-pretty-entities t
+		org-ellipsis "…"))
