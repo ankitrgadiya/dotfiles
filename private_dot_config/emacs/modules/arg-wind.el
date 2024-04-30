5c8dbb120152672a00491ca4e8414ce241a4db02
Ankit R Gadiya <ankit.gadiya@rapyuta-robotics.com>
2024-04-30 18:21:30 +0530

    emacs: remove select-window action for compilation-mode

--- private_dot_config/emacs/modules/arg-wind.el
+++ private_dot_config/emacs/modules/arg-wind.el
@@ -22,14 +22,17 @@
 		`(("\\**eshell\\*"
 		   (display-buffer-at-bottom)
 		   (dedicated . t))
-		  ((or . ((derived-mode . help-mode)
-				  (derived-mode . compilation-mode)
-				  "\\*Agenda Commands\\*"))
+		  ((derived-mode . help-mode)
 		   (display-buffer-reuse-mode-window
 			display-buffer-below-selected)
 		   (body-function . select-window)
 		   (window-height . 0.3)
 		   (window-parameters . ((mode-line-format . none)))
+		   (dedicated . t))
+		  ((derived-mode . compilation-mode)
+		   (display-buffer-reuse-mode-window
+			display-buffer-below-selected)
+		   (window-height . 0.3)
 		   (dedicated . t))
 		  ((or . ((derived-mode . magit-status-mode)
 				  "\\*Org Agenda\\*"))
