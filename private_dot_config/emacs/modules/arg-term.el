1f44679cc0821f912445fd2e6996652c6882ea40
Ankit R Gadiya <git@argp.in>
2024-06-28 00:44:11 +0530

    emacs: auto-delete eat buffer on exit

--- private_dot_config/emacs/modules/arg-term.el
+++ private_dot_config/emacs/modules/arg-term.el
@@ -14,7 +14,14 @@
 (use-package eat
   :ensure t
   :config
+  ;; Kill Eat buffer on exit
+  (add-hook 'eat-exit-hook
+			(lambda (_)
+			  (kill-buffer (current-buffer))))
+
+  ;; Enable Eat mode in Eshell buffer
   (eat-eshell-mode)
   (require 'evil)
+  (evil-set-initial-state 'eshell-mode 'emacs)
   (evil-define-key 'normal 'global
 	(kbd "<leader>st") 'eat))
