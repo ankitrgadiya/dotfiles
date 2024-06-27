95b77ab7afd4c36877ab41af005e30d92120a3a8
Ankit R Gadiya <git@argp.in>
2024-06-28 00:37:47 +0530

    emacs: remove erc and mastodon.el config

--- private_dot_config/emacs/modules/arg-pers.el
+++ private_dot_config/emacs/modules/arg-pers.el
@@ -5,45 +5,6 @@
   (setq epg-gpg-program "gpg2"
 		auth-sources '((:source "~/.config/emacs/secrets/authinfo.gpg"))))
 
-;; `erc' is the built-in IRC Client for Emacs.
-(use-package erc
-  :defer t
-  :config
-  (setq erc-autojoin-channels-alist '(("irc.libera.chat"
-									   "#emacs" "#emacsconf" "#systemcrafters"
-									   "#org-mode" "#guile" "#go-nuts" "#debian"))
-        erc-use-auth-source-for-nickserv-password t
-		erc-track-shorten-start 9
-		erc-kill-buffer-on-part t
-		erc-fill-column 120
-		erc-fill-function 'erc-fill-static
-		erc-fill-static-center 20
-		erc-kill-buffer-on-part t
-		erc-kill-server-buffer-on-quit t
-		erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "AWAY")
-		erc-track-exclude-server-buffer t
-		erc-track-visibility nil)
-
-  (defun arg-erc-libera ()
-	(interactive)
-	(erc-tls
-	 :server "irc.libera.chat"
-	 :port 6697
-	 :nick "argp"))
-
-  (require 'evil)
-  (evil-define-key 'normal 'global
-	(kbd "<leader>ic") #'arg-erc-libera
-	(kbd "<leader>ib") 'erc-switch-to-buffer))
-
-;; `mastodon' is the Mastodon Client for Emacs.
-(use-package mastodon
-  :ensure t
-  :defer t
-  :config
-  (setq mastodon-instance-url "https://emacs.ch"
-        mastodon-active-user "ankit")
-  (evil-set-initial-state 'mastodon-mode 'emacs))
 
 ;; `empv' is the Media manager in Emacs (backed by the MPV player).
 (use-package empv
